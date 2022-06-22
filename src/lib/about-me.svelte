d01b80da88a1e76d28108034f215fd10716b03a1
hendrik.loewe <hendrikloewe3@gmail.com>
2022-06-22 22:25:38 +0200

    Add about-me section with first content

---
+++ src/lib/about-me.svelte
@@ -0,0 +1,30 @@
+<h2 class="section-title">About Me</h2>
+<div class="card">
+	<div class="content" />
+	<div class="portrait">
+		<img src="/portfolio/images/portrait.jpg" alt="Portrait of myself" />
+	</div>
+</div>
+
+<style lang="scss">
+	.card {
+		height: 80%;
+		width: 100%;
+		display: grid;
+		grid-template-columns: 1fr auto;
+        grid-template-rows: 100%;
+		background-color: $primary;
+		border-radius: 2rem;
+		box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25);
+	}
+
+	.portrait {
+		height: 100%;
+        
+		> img {
+			height: 100%;
+			border-radius: 0rem 2rem 2rem 0rem;
+            object-fit: cover;
+		}
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,19 +4,32 @@
 
 <script lang="ts">
 	import Hero from '$lib/hero.svelte';
+	import AboutMe from '$lib/about-me.svelte';
 </script>
 
 <section class="hero-section">
 	<Hero />
 </section>
 
+<section class="about-me-section">
+	<AboutMe />
+</section>
+
 <style lang="scss">
 	section {
 		width: 100%;
 		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
 	}
 
 	.hero-section {
 		position: relative;
+	}
+
+	.about-me-section {
+		padding-inline: 10rem;
 	}
 </style>
